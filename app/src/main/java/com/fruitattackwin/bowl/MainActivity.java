52f0b707a31bc1926e011c4ba718f6763cf90523
Sanobe1 <valentenkos@gmail.com>
2021-07-22 18:14:36 +0300

    bubilda

--- app/src/main/java/com/fruitattackwin/bowl/MainActivity.java
+++ app/src/main/java/com/fruitattackwin/bowl/MainActivity.java
@@ -4,10 +4,8 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
